7d655e735fcc862752436f31144a5dc032ddf11b
Legiani <kuba.bednar@seznam.cz>
2017-03-26 12:07:55 +0200

    Fix droid Sqlite

--- Droid/FileHelper.cs
+++ Droid/FileHelper.cs
@@ -1,25 +1,18 @@
 ﻿using System;
 using System.IO;
-using Kontakty.iOS;
+using Kontakty;
+using Kontakty.Droid;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(FileHelper))]
-namespace Kontakty.iOS
+namespace Kontakty.Droid
 {
-
 	public class FileHelper : IFileHelper
 	{
 		public string GetLocalFilePath(string filename)
 		{
-			string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-			string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
-
-			if (!Directory.Exists(libFolder))
-			{
-				Directory.CreateDirectory(libFolder);
-			}
-
-			return Path.Combine(libFolder, filename);
+			string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+			return Path.Combine(path, filename);
 		}
 	}
 }
